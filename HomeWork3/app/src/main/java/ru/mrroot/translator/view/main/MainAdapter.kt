a885b9addaa5b6b96991be65c5f594d66812aa53
dimov-k <dimov.k.k@gmail.com>
2021-10-29 11:09:06 +0300

    HomeWork3

---
+++ HomeWork3/app/src/main/java/ru/mrroot/translator/view/main/MainAdapter.kt
@@ -0,0 +1,51 @@
+package ru.mrroot.translator.view.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.mrroot.translator.databinding.ActivityMainRvItemBinding
+import ru.mrroot.translator.model.entity.Word
+import ru.mrroot.translator.utils.getTranslationsString
+
+class MainAdapter(private var itemClickListener: ((Word) -> Unit)?) :
+    RecyclerView.Adapter<MainAdapter.RecyclerItemViewHolder>() {
+
+    private var data: List<Word> = arrayListOf()
+
+    fun setData(data: List<Word>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerItemViewHolder {
+        return RecyclerItemViewHolder(
+            ActivityMainRvItemBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerItemViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class RecyclerItemViewHolder(private val vb: ActivityMainRvItemBinding) :
+        RecyclerView.ViewHolder(vb.root) {
+
+        private lateinit var data: Word
+
+        init {
+            itemView.setOnClickListener { itemClickListener?.invoke(data) }
+        }
+
+        fun bind(word: Word) {
+            this.data = word
+            vb.headerTextviewRv.text = word.text
+            vb.descriptionTextviewRv.text = word.getTranslationsString()
+            vb.transcriptionTextviewRv.text = word.translations?.get(0)?.transcription
+        }
+    }
+}
